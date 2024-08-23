37ab21d15a8270d38f7715a7d2e54ce5b38ef016
Hussain <hkosaimi@gmail.com>
2024-08-23 15:15:40 +0300

    try

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    port: 3000,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
-  base: "/admin",
+  base: "https://adminuseracc.vercel.app/admin",
 });
