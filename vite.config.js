2d41268e3a5893daa685faf878f962965d263699
Hussain <hkosaimi@gmail.com>
2024-08-23 14:59:41 +0300

    fixing port

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+
   css: {
     postcss: {
       plugins: [tailwindcss()],
