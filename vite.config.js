dd87ef89216296d2288c5f309ed2050f00f14384
Hussain <hkosaimi@gmail.com>
2024-08-23 15:44:20 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
